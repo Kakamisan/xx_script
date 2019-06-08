35936fff650057abbce3992291ca957d5f90225d
kakami <kakamisound@outlook.com>
2019-06-08 13:47:10 +0800

    开始写lo脚本

--- xsp/src/define.lua
+++ xsp/src/define.lua
@@ -8,7 +8,21 @@ swc_sample		= 1
 
 text_sample		= 1
 
-view_sample		= 1
+view_home			= 1		--主页
+view_slc_dg_type	= 2		--选择副本类型：每日，主线，活动等
+view_slc_dg_main	= 3		--选择主线副本，6-8，5-8e等
+view_slc_team		= 4		--选择队伍，队伍1，队伍5等
+view_bt_slc_acn		= 5		--战斗中-选择行动
+view_bt_playing		= 6		--战斗中-播报中
+view_bt_report		= 7		--战斗中-战斗经验结算
+view_bt_waiting		= 8		--战斗中-打捞老婆
+view_bt_get_waifu	= 9		--战斗中-捞到老婆
+view_bt_next		= 10	--战斗中-下一波
+view_bt_moving		= 11	--战斗中-赶路到下一波
+view_bt_over		= 12	--战斗中-总结算界面
+view_stop_repeat	= 13	--选择是否停止反复战斗
+view_ctn_mission	= 14	--主页-继续远征
+
 
 target_default	= 0		--默认target
 target_sample	= 1
