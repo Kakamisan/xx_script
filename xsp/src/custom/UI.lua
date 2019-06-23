959183f9814401afca7d745493970a05c5cdc07a
kakami <kakamisound@outlook.com>
2019-06-23 12:57:27 +0800

    增加配置选择

--- xsp/src/custom/UI.lua
+++ xsp/src/custom/UI.lua
@@ -1,10 +1,36 @@
 local content = getUIContent("ui.json")		--默认的ui
 
-if pos.cw >= 720 then
-	content = getUIContent("ui2.json")		--当前设备分辨率为720以上时，输出ui2
-end
+--if pos.cw >= 720 then
+--	content = getUIContent("ui2.json")		--当前设备分辨率为720以上时，输出ui2
+--end
 
 local ret,UI = showUI(content)				--显示UI
+
+if ret ~= 1 then
+	lua_exit()
+end
+
+local cfgslclist = {
+	[0] = "ui1.json",
+	[1] = "ui2.json",
+	[2] = "ui3.json",
+	[3] = "ui4.json",
+	[4] = "ui5.json",
+	[5] = "ui6.json",
+	[6] = "ui7.json",
+	[7] = "ui8.json",
+	[8] = "ui9.json",
+	[9] = "ui10.json",
+	[10] = "ui11.json",
+	[11] = "ui12.json",
+	[12] = "ui13.json"
+}
+
+dlog(cfgslclist[tonumber(UI.cfgslc)])
+
+content = getUIContent(cfgslclist[tonumber(UI.cfgslc)])
+
+local ret,UI = showUI(content)
 
 if ret ~= 1 then
 	lua_exit()
